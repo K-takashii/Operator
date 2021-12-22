599575166a59e9f8cfcfb04c0bc988dbdb4c54c7
“K-takashii” <nanase.sigre.1627.yorushika@gmail.com>
2021-12-22 19:20:07 +0900

    Initial commit

---
+++ Operator.java
@@ -0,0 +1,12 @@
+class Operator {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 2;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y); //掛け算
+        System.out.println(x / y); //割り算
+        System.out.println(x % y); //剰余(割り算の余り)
+    }
+}
